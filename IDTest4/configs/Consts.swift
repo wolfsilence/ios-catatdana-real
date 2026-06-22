0858aa30fbfd3ed6931e7cd95e1e9130d18654a1
Jacky <jacky@JackydeMacBook-Pro.local>
2026-06-22 20:04:44 +0800

    feat: 修改配置

--- IDTest4/configs/Consts.swift
+++ IDTest4/configs/Consts.swift
@@ -11,8 +11,8 @@ class Consts {
     static let encry = true
     static let bURL = "https://cdk1i1os.com"
     
-    static let appStoreId = "6781208634"// TODO
-    static let afDevKey = "bdh84JPS3yPAQYnzc2kbk9"// TODO
+    static let appStoreId = "6781208634"
+    static let afDevKey = "bdh84JPS3yPAQYnzc2kbk9"
 
     static let ppUrl = "https://cdk1i1os.com/privacy.html"
     static let slUrl = "https://sdk.thailand.accuauth.com/silent-liveness-h5/index.html#"
